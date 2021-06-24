26ee205e1a9b701e50699240cd10567f47867df0
Shivam27777 <shivam27777@gmail.com>
2021-06-24 16:13:42 +0530

    form styled

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
 import { setUserSession } from "../Utils/Common";
 
@@ -6,62 +6,87 @@ import './Login.css';
 
 const Login = (props) => {
   const [loading, setLoading] = useState(false);
-  const username = useFormInput("");
+  const email = useFormInput("");
   const password = useFormInput("");
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    document.getElementsByClassName("outer")['0'].style.height=(window.innerHeight-65)+"px";
+  })
+
 
   // handle button click of login form
   const handleLogin = () => {
     setError(null);
     setLoading(true);
     axios
-      .post("http://localhost:4000/user/signin", {
-        username: username.value,
+      .post("http://localhost:4000/api/auth/login", {
+        email: email.value,
         password: password.value,
       })
       .then((response) => {
         setLoading(false);
+        console.log(response.data);
         setUserSession(response.data.token, response.data.user);
         props.history.push("/dashboard");
       })
       .catch((error) => {
         setLoading(false);
-        if (error.response.status === 401)
-          setError(error.response.data.message);
+        if(error.response.status === 400)
+          setError("Fill all the fields!!");
+        else if (error.response.status === 401)
+          setError("Email address or password is incorrect");
         else setError("Something went wrong. Please try again later.");
       });
   };
 
   return (
     <div className="outer">
-    <div className="container">
+    <div id="container">
+    <div id="form-container">
+      <div id="form-shadow">
+      <div id="loginHeading">
       <h1>Login</h1>
-      <br />
-      <br />
-      <div>
-        {/* <label className="labelcontainer" >Username</label> */}
-        <input className="inputcontainer" placeholder="Username"  type="text" {...username}  />
+      </div>
+      
+      <div className ="input-styles">
+        
+        {/* <label className="labelcontainer" >Email ID</label> */}
+        <input className="box" type="text" placeholder="E-Mail"  {...email}  />
+        
       </div>
       <div style={{ marginTop: 10 }}>
+      <div className="input-styles">
         {/* <label className="labelcontainer" >Password</label> */}
-        <input className="inputcontainer"  placeholder="Password" type="password" {...password} />
+        <input className="box" type="password" placeholder="Password" {...password} />
+        </div>
       </div>
       {error && (
         <>
-          <small style={{ color: "red" }}>{error}</small>
-          <br />
+          <small style={{ color: "red", textAlign:"center", marginTop:"10px", fontSize:"16px" }}>{error}</small>
         </>
       )}
       <br />
+      <div className="input-styles submit-btn">
       <input
-      className="btn"
         type="button"
+        id="login"
+        className="submit"
         value={loading ? "Loading..." : "Login"}
         onClick={handleLogin}
         disabled={loading}
       />
-      <br />
+      
+      </div>
+      <div id="registration-page">
+      <a href="registration.html">
+        <h4>Not Registered? Click here to Register</h4>
+      </a>
     </div>
+    </div>
+    </div>
+    </div>
+
     </div>
   );
 }
--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react'
 import axios from "axios";
-
 
 import './Register.css';
 
 const Register = (props) => {
   const [user, setUser] = useState({
-    name: "",
-    username: "",
+    email: "",
     password: "",
     cpassword: "",
-    authority: "admin",
+    authority: "ADMIN",
   });
   const [error, setError] = useState(null);
 
@@ -22,11 +20,18 @@ const Register = (props) => {
     setUser({ ...user, [name]: value });
   };
 
+  useEffect(() => {
+    if(user.cpassword !== user.password)
+      setError("Passwords don't match");
+    else
+      setError("");
+    document.getElementsByClassName("box inline-box right-box")['0'].style.height=(window.innerHeight-65)+"px";
+  }, [user.password, user.cpassword])
+
   const handleRegister = () => {
     axios
-      .post("http://localhost:4000/user/register", {
-        name: user.name,
-        username: user.username,
+      .post("http://localhost:4000/api/auth/register-admin", {
+        email: user.email,
         password: user.password,
         cpassword: user.cpassword,
         authority: user.authority,
@@ -36,75 +41,91 @@ const Register = (props) => {
         console.log(res.data);
       })
       .catch((err) => {
-        setError(err.response.data.message);
+        if(err.response.status===400) {
+          setError("Some of the fields are missing!!");
+        }
+        else if(err.response.status===500) {
+          setError("Server failed");
+        }
       });
   };
 
   return (
-    <div className="Rcontainer">
-      <p className="form-title">Register Here!!</p>
-      <form className="register-form" id="register-form">
-      
-        <div className="form-group">
-          {/* <label className="Rlabelcontainer" htmlFor="name">Name</label> */}
-          <input className="Rinputcontainer" placeholder="Name"
+    <div id="container">
+      <div id="success"></div>
+      <div id="form-container">
+      <form className="register-form" id="registerForm">
+        <div id="form-shadow">
+          <div id ="registerHeading">
+      <h1 >Register Here!!</h1>
+      </div>
+        <div className="input-styles input-width">
+          {/* <label className="Rlabelcontainer" htmlFor="name">First Name</label> */}
+          <input className="box inline-box"
             type="text"
             value={user.name}
             onChange={handleInputs}
+            placeholder="First Name"
             name="name"
-            id="name"
+            id="firstName"
             required
           />
         </div>
-        <div className="form-group">
-          {/* <label className="Rlabelcontainer" htmlFor="username">Username</label> */}
-          <input className="Rinputcontainer"  placeholder="Username"
+        <div className="input-styles ">
+          {/* <label className="box inline-box" htmlFor="email">Email ID</label> */}
+          <input className="box inline-box"
             type="text"
-            value={user.username}
+            value={user.email}
             onChange={handleInputs}
-            name="username"
-            id="username"
+            name="email"
+            id="email"
+            placeholder="E-Mail"
             required
           />
         </div>
-        <div className="form-group">
+        <div className="input-styles input-width">
           {/* <label className="Rlabelcontainer" htmlFor="password">Password</label> */}
-          <input className="Rinputcontainer" placeholder="Password"
+          <input className="box inline-box"
             type="password"
             value={user.password}
             onChange={handleInputs}
             name="password"
+            placeholder="Password"
             id="password"
             required
           />
-        </div>
-        <div className="form-group">
-          {/* <label className="Rlabelcontainer" htmlFor="cpassword">Confirm Password</label> */}
-          <input className="Rinputcontainer"   placeholder="Confirm Password"
+          <input className="box inline-box right-box"
             type="password"
             value={user.cpassword}
             onChange={handleInputs}
             name="cpassword"
+            placeholder="Confirm Password"
             id="cpassword"
             required
           />
         </div>
+       
         {error && (
-        <>
-          <small style={{ color: "red" }}>{error}</small>
-          <br />
-        </>
+          <small style={{ color: "red", textAlign:"center", fontSize:"16px", marginTop:"10px" }}>{error}</small>
       )}
       <br />
-        <div className="form-group form-button">
-          <input 
+        <div className="input-styles submit-btn">
+          <input
+            id ="submitDetails"
             type="button"
             className="form-submit"
             onClick={handleRegister}
             value="Register"
           />
         </div>
+        </div>
       </form>
+      </div>
+      <div id="login-page">
+      <a href="login.html">
+          <h4>Already Registered? Click here to Login</h4>
+      </a>
+  </div>
     </div>
   );
 };
